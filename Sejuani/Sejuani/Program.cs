9065da38d7fd9521b35dc44c695e25a73f8f3e6a
Heisenbergg <gotengo4@gmail.com>
2015-02-06 04:20:24 -0300

    att

--- Sejuani/Sejuani/Program.cs
+++ Sejuani/Sejuani/Program.cs
@@ -17,7 +17,9 @@ namespace sejuani
         public static Orbwalking.Orbwalker Orbwalker; //Declara o orbwalker
         private static Obj_AI_Hero Player { get { return ObjectManager.Player; } } // Declara o ObjectManager.Player é necessario para trabalharmos com um champion
 
-        public static Menu Haisenberg; //declara o menu
+        public static Menu Menu; //declara o menu
+        
+        private static Spell Q, W, E, R;  //Declara habilidades
 
         static void Main(string[] args)
         {
@@ -32,25 +34,44 @@ namespace sejuani
             if (Player.ChampionName != "Sejuani") //Verifica se é o champion correto, se não, não executa o script
                 return;
 
-            Haisenberg = new Menu("Haisenberg" + ChampName, ChampName, true);
 
+            Q = new Spell(SpellSlot.Q, 650);
+            W = new Spell(SpellSlot.W, 350);
+            E = new Spell(SpellSlot.E, 1000);
+            R = new Spell(SpellSlot.R, 1175);
 
             //Criar o Maenu
-            Haisenberg.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
-            Orbwalker = new Orbwalking.Orbwalker(Haisenberg.SubMenu("Orbwalker"));
+            Menu = new Menu("Heisenberg" + ChampName, ChampName, true);
+
+            //Menu Orbwalker
+            Menu orbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
 
             //Target selector e menu
             var ts = new Menu("TargetSelector", "Target Selector");
             TargetSelector.AddToMenu(ts);
-            Haisenberg.AddSubMenu(ts);
-            //Nosso Menu
-            Haisenberg.AddItem(new MenuItem("NossoMenu1", "Opção1").SetValue(true));
-            Haisenberg.AddItem(new MenuItem("NossoMenu2", "Opção2").SetValue(new KeyBind("Y".ToCharArray()[0], KeyBindType.Toggle, true)));
+            Menu.AddSubMenu(ts);
 
+
+            //Habilidades Menu
+            Menu spellMenu = Menu.AddSubMenu(new Menu("Habilidades", "Habilidades"));
+            spellMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
+            spellMenu.AddItem(new MenuItem("useW", "Use W").SetValue(true));
+            spellMenu.AddItem(new MenuItem("useE", "Use E").SetValue(true));
+            spellMenu.AddItem(new MenuItem("useR", "Use R").SetValue(true));
+            
+            
+            
+            
+            
+            
+            
+            
+            
             //Faz o menu ficar visivel
-            Haisenberg.AddToMainMenu();
+            Menu.AddToMainMenu();
 
-            Game.PrintChat("Haisenberg" + ChampName + "Injetado");
+            Game.PrintChat("Heisenberg" + ChampName + "Injetado");
             Game.PrintChat("<font color =\"#87CEEB\">Heisenberg Sejuani</font>");
 
 
